63d435852c96985d5eb405debff2bb2d39a3b44f
Abhay Singh <abhay5349singh@gmail.com>
2021-12-12 11:17:31 +0530

    Add files via upload

---
+++ Weekly Contest 271/2105. Watering Plants II.cpp
@@ -0,0 +1,39 @@
+Question Link: https://leetcode.com/problems/insert-interval/
+
+class Solution {
+public:
+    int minimumRefill(vector<int>& a, int capacityA, int capacityB) {
+        int n=a.size();
+        
+        int ca=capacityA, cb=capacityB;
+        
+        int ans=0;
+        
+        int i=0, j=n-1;
+        while(i<j){
+            if(ca>=a[i]){
+                ca-=a[i];
+            }else{
+                ca=capacityA;
+                ca-=a[i];
+                
+                ans++;
+            }
+            
+            if(cb>=a[j]){
+                cb-=a[j];
+            }else{
+                cb=capacityB;
+                cb-=a[j];
+                
+                ans++;
+            }
+            
+            i++, j--;
+        }
+        
+        if(i==j && a[i] > max(ca,cb)) ans++;
+        
+        return ans;
+    }
+};
