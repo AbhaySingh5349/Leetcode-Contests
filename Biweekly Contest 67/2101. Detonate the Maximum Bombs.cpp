1ac2eb68008250eaa44b7ca006851dfe2e06ecf1
Abhay Singh <abhay5349singh@gmail.com>
2021-12-14 17:17:23 +0530

    Update 2101. Detonate the Maximum Bombs.cpp

--- Biweekly Contest 67/2101. Detonate the Maximum Bombs.cpp
+++ Biweekly Contest 67/2101. Detonate the Maximum Bombs.cpp
@@ -1,4 +1,4 @@
-Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+Question Link: https://leetcode.com/problems/detonate-the-maximum-bombs/
 
 class Solution {
 public:
