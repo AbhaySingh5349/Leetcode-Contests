fb2197d0ef666ee6b8ffca77e6b46ea9b720e0ef
Abhay Singh <abhay5349singh@gmail.com>
2021-12-26 12:59:13 +0530

    Add files via upload

---
+++ Weekly Contest 273/2120. Execution of All Suffix Instructions Staying in a Grid.cpp
@@ -0,0 +1,34 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    
+    int count(int n, int i, int j, string &s, int idx){
+        if(i<0 || j<0 || i==n || j==n) return -1;
+        if(idx==s.length()) return 0;
+        
+        char dir=s[idx];
+        
+        int c=1;
+        if(dir=='L'){
+            c+=count(n,i,j-1,s,idx+1);
+        }else if(dir=='R'){
+            c+=count(n,i,j+1,s,idx+1);
+        }else if(dir=='U'){
+            c+=count(n,i-1,j,s,idx+1);
+        }else if(dir=='D'){
+            c+=count(n,i+1,j,s,idx+1);
+        }
+        return c;
+    }
+    
+    vector<int> executeInstructions(int n, vector<int>& start, string s) {
+        int m=s.length();
+        vector<int> ans(m,0);
+        
+        for(int i=0;i<m;i++){
+            ans[i]=count(n,start[0],start[1],s,i);
+        }
+        return ans;
+    }
+};
