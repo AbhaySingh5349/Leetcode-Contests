bcda40cdf43d6d750e07b7183977263d13548a9f
Abhay Singh <abhay5349singh@gmail.com>
2021-12-26 18:06:50 +0530

    Add files via upload

---
+++ Biweekly Contest 68/2116. Check if a Parentheses String Can Be Valid.cpp
@@ -0,0 +1,51 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    
+    bool leftToRight(string s, string locked){
+        int open=0, close=0, free=0;
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            if(locked[i]=='1'){
+                if(ch=='('){
+                    open++;
+                }else{
+                    close++;
+                }
+            }else{
+                free++;
+            }
+            if(close>open+free) return false;
+        }
+        
+        return true;
+    }
+    
+    bool rightToLeft(string s, string locked){
+        int open=0, close=0, free=0;
+        
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s[i];
+            if(locked[i]=='1'){
+                if(ch=='('){
+                    open++;
+                }else{
+                    close++;
+                }
+            }else{
+                free++;
+            }
+            if(open>close+free) return false;
+        }
+        
+        return true;
+    }
+    
+    bool canBeValid(string s, string locked) {
+        if(s.length()%2==1) return false;
+        
+        return (leftToRight(s,locked) && rightToLeft(s,locked));
+    }
+};
