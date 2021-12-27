de498bc72bcaa92e370554c8d08856c716148b9b
Abhay Singh <abhay5349singh@gmail.com>
2021-12-27 16:09:44 +0530

    Update 2115. Find All Possible Recipes from Given Supplies.cpp

--- Biweekly Contest 68/2115. Find All Possible Recipes from Given Supplies.cpp
+++ Biweekly Contest 68/2115. Find All Possible Recipes from Given Supplies.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/
 
+// Approach 1:
+
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipies, vector<vector<string>>& ing, vector<string>& supplies) {
@@ -28,6 +30,48 @@ public:
                 }
             }
             if(possible==false) break;
+        }
+        return ans;
+    }
+};
+
+
+// Approach 2:
+
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipies, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        
+        map<string,int> indegree; // to store the indegree of all recipes
+        
+        map<string,vector<string>> graph;
+        
+        for(int i=0;i<recipies.size();i++){
+            string rec=recipies[i];
+            
+            for(int j=0;j<ingredients[i].size();j++){
+                string ing=ingredients[i][j];
+                
+                graph[ing].push_back(rec);
+                indegree[rec]++;
+            }
+        }
+        
+        queue<string> q;
+        for(int i=0;i<supplies.size();i++) q.push(supplies[i]);
+        
+        vector<string> ans;
+        while(q.size()>0){
+            string sup=q.front();
+            q.pop();
+            
+            for(auto rec : graph[sup]){
+                indegree[rec]--;
+                if(indegree[rec] == 0){
+                    q.push(rec);
+                    ans.push_back(rec);
+                }
+            }
         }
         return ans;
     }
