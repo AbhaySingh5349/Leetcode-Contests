04af84cc693eb2b84d2b77634c14830d30c13e0a
Abhay Singh <abhay5349singh@gmail.com>
2021-12-27 13:06:41 +0530

    Add files via upload

---
+++ Weekly Contest 273/2122. Recover the Original Array.cpp
@@ -0,0 +1,40 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    
+    bool possible(multiset<int> st, int k, vector<int> &ans){
+        while(st.size()>0){
+            int low=*st.begin();
+            int high=low+2*k;
+            
+            if(st.find(high)==st.end()) return false;
+            
+            ans.push_back((low+high)/2);
+            
+            // erase iterators and not elements as there might be multiple occurances of element
+            st.erase(st.begin());
+            auto it=st.find(high);
+            st.erase(it);
+        }
+        return true;
+    }
+    
+    vector<int> recoverArray(vector<int> &a) {
+        sort(a.begin(),a.end()); 
+        
+        multiset<int> st(a.begin(),a.end());
+        
+        int low=a[0];
+        for(int i=1;i<a.size();i++){
+            vector<int> ans;
+            
+            int high=a[i];
+            if((high-low)%2==0){
+                int k=(high-low)/2;
+                if(k>0 && possible(st,k,ans)) return ans;
+            }
+        }
+        return {};
+    }
+};
