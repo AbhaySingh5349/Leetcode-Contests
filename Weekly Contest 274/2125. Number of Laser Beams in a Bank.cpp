7fb5c7574cef0dd613a7ccc37ae82d045fe70573
Abhay Singh <abhay5349singh@gmail.com>
2022-01-02 09:39:55 +0530

    Add files via upload

---
+++ Weekly Contest 274/2125. Number of Laser Beams in a Bank.cpp
@@ -0,0 +1,23 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    int numberOfBeams(vector<string> &grid) {
+        int n=grid.size(), m=grid[0].size();
+        
+        vector<int> v;
+        
+        for(int i=0;i<n;i++){
+            int c=0;
+            for(int j=0;j<m;j++){
+                c += (grid[i][j]=='1');
+            }
+            if(c!=0) v.push_back(c);
+        }
+        
+        int c=0;
+        for(int i=1;i<v.size();i++) c += (v[i]*v[i-1]);
+        
+        return c;
+    }
+};
