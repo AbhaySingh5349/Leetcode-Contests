c1b34d42831219a7668d2556f2702d5a5282a8da
Abhay Singh <abhay5349singh@gmail.com>
2022-01-09 12:52:26 +0530

    Add files via upload

---
+++ Biweekly Contest 69/2129. Capitalize the Title.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    string capitalizeTitle(string s) {
+        int i=0, n=s.length();
+        
+        string ans="";
+        while(i<n){
+            int len=0;
+            string sub="";
+            while(i<n && isalpha(s[i])){
+                sub += (s[i]>='A' && s[i]<='Z' ? s[i]-'A'+'a' : s[i]);
+                i++;
+            }
+            
+            if(sub.length()>2) sub[0] = sub[0]-'a'+'A';
+            
+            ans += sub + " ";
+            
+            i++;
+        }
+        return ans.substr(0,ans.length()-1);
+    }
+};
