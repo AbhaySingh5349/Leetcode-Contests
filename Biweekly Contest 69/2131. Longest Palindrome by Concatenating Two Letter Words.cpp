b0c28b1a918d61ed02f5349a01b0fbebb6690e4c
Abhay Singh <abhay5349singh@gmail.com>
2022-01-09 13:01:41 +0530

    Add files via upload

---
+++ Biweekly Contest 69/2131. Longest Palindrome by Concatenating Two Letter Words.cpp
@@ -0,0 +1,36 @@
+Question Link: https://leetcode.com/problems/capitalize-the-title/
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        map<string,int> mp;
+        
+        for(int i=0;i<words.size();i++) mp[words[i]]++;
+        
+        int len=0, extra=false;
+        map<string,int> :: iterator it;
+        for(it=mp.begin();it!=mp.end();it++){
+            string s1=it->first;
+            int f1=it->second;
+            string s2=s1;
+            reverse(s2.begin(),s2.end());
+            
+            if(mp.find(s2)!=mp.end()){ // checking if reverse of current string exist or not
+                int f2=mp[s2];
+                
+                if(s1==s2){
+                    len += (f1>1 ? (f1/2)*4 : 0);
+                    if(f1%2==1) extra=true; // ['dd','aa',dd','bb'] --> we can use 1 of the pair occuring once as mid of our palindrome
+                }else{
+                    len += min(f1,f2)*4;
+                } 
+                mp.erase(s2);
+            }
+            mp.erase(s1);
+        }
+        
+        if(extra) len+=2;
+        
+        return len;
+    }
+};
