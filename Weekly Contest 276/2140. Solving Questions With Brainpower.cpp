b6e652e699d3a6a1c1d65e2f9de9c39f3d37bcef
Abhay Singh <abhay5349singh@gmail.com>
2022-01-16 18:57:48 +0530

    Update 2140. Solving Questions With Brainpower.cpp

--- Weekly Contest 276/2140. Solving Questions With Brainpower.cpp
+++ Weekly Contest 276/2140. Solving Questions With Brainpower.cpp
@@ -1,4 +1,4 @@
-Question Link:https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+Question Link: https://leetcode.com/problems/solving-questions-with-brainpower/
 
 class Solution {
 public:
