b5441714fc0d02fc7150fecb10e7c9acd1f01b3d
Abhay Singh <abhay5349singh@gmail.com>
2022-01-16 18:57:26 +0530

    Add files via upload

---
+++ Weekly Contest 276/2140. Solving Questions With Brainpower.cpp
@@ -0,0 +1,22 @@
+Question Link:https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+
+class Solution {
+public:
+    
+    #define ll long long
+    
+    long long mostPoints(vector<vector<int>> &a) {
+        int n=a.size();
+        
+        ll int dp[n];
+        dp[n-1]=a[n-1][0];
+        
+        ll int ans=dp[n-1];
+        for(int i=n-2;i>=0;i--){
+            dp[i]=max(dp[i+1],a[i][0]+(i+a[i][1]+1 < n ? dp[i+a[i][1]+1]:0));
+            if(dp[i]>ans) ans=dp[i];
+        }
+        
+        return ans;
+    }
+};
