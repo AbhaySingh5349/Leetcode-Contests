522e104882fb95e186c7d4027d94496392c748b0
Abhay Singh <abhay5349singh@gmail.com>
2022-01-16 18:53:52 +0530

    Add files via upload

---
+++ Weekly Contest 276/2139. Minimum Moves to Reach Target Score.cpp
@@ -0,0 +1,20 @@
+Question Link: https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+
+class Solution {
+public:
+    
+    int minMoves(int n, int maxDoubles) {
+        int c=0;
+        while(n>1){
+            if(maxDoubles>0 && n%2==0){
+                n/=2;
+                maxDoubles--;
+            }else{
+                n--;
+            }
+            c++;
+            if(maxDoubles==0) break;
+        }
+        return c+(n-1); 
+    }
+};
