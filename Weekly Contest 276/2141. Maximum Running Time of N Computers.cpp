2eb7addf10b8647b72eddd65bebe3a8a22f51b88
Abhay Singh <abhay5349singh@gmail.com>
2022-01-16 19:23:33 +0530

    Add files via upload

---
+++ Weekly Contest 276/2141. Maximum Running Time of N Computers.cpp
@@ -0,0 +1,39 @@
+Question Link: https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+
+class Solution {
+public:
+    
+    #define ll long long
+    
+    bool possible(int n, vector<int> &bat, ll int hrs){
+        ll int s=0;
+        for(int i=0;i<bat.size();i++){
+            s += bat[i];
+            if(s>=hrs){
+                s-=hrs;
+                n--;
+            }
+            if(n==0) return true;
+        }
+        return false;
+    }
+    
+    long long maxRunTime(int n, vector<int> &bat) {
+        ll int l=1, r=0;
+        
+        for(int i=0;i<bat.size();i++) r += bat[i];
+        r/=n;
+        
+        sort(bat.begin(),bat.end());
+        while(l<r){
+            ll int m=l+(r-l+1)/2;
+            
+            if(possible(n,bat,m)){
+                l=m;
+            }else{
+                r=m-1;
+            }
+        }
+        return l;
+    }
+};
