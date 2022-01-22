70188f66295cac5f05286f2a6ab697e878f2430a
Abhay Singh <abhay5349singh@gmail.com>
2022-01-22 23:06:32 +0530

    Add files via upload

---
+++ Biweekly Contest 70/2144. Minimum Cost of Buying Candies With Discount.cpp
@@ -0,0 +1,27 @@
+Question Link: https://leetcode.com/problems/license-key-formatting/
+
+class Solution {
+public:
+    int minimumCost(vector<int> &a) {
+        int n=a.size();
+        priority_queue<int> pq;
+        for(int i=0;i<n;i++) pq.push(a[i]);
+        
+        int c=0;
+        while(pq.size()>0){
+            int f=pq.top();
+            pq.pop();
+            
+            c+=f;
+            
+            if(pq.size()>0){
+                int s=pq.top();
+                pq.pop();
+                
+                c+=s;
+            }
+            if(pq.size()>0) pq.pop();
+        }
+        return c;
+    }
+};
