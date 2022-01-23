c270ff196abaf63a62942b4b5eff68595bef87fe
Abhay Singh <abhay5349singh@gmail.com>
2022-01-23 12:22:30 +0530

    Add files via upload

---
+++ Weekly Contest 277/2150. Find All Lonely Numbers in the Array.cpp
@@ -0,0 +1,17 @@
+Question Link: https://leetcode.com/problems/license-key-formatting/
+
+class Solution {
+public:
+    vector<int> findLonely(vector<int> &a) {
+        int n=a.size();
+        map<int,int> mp;
+        for(int i=0;i<n;i++) mp[a[i]]++;
+        
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(mp[a[i]]>1) continue;
+            if(mp.find(a[i]-1)==mp.end() && mp.find(a[i]+1)==mp.end()) ans.push_back(a[i]);
+        }
+        return ans;
+    }
+};
