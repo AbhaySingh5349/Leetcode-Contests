8fbeb565ec9827ba76cc00f9708318e821c83568
Abhay Singh <abhay5349singh@gmail.com>
2022-01-23 11:59:00 +0530

    Add files via upload

---
+++ Weekly Contest 277/2148. Count Elements With Strictly Smaller and Greater Elements.cpp
@@ -0,0 +1,27 @@
+Question Link: https://leetcode.com/problems/license-key-formatting/
+
+class Solution {
+public:
+    int countElements(vector<int> &a) {
+        int n=a.size();
+        
+        int c=0;
+    /*    for(int i=0;i<n;i++){
+            bool small=false, large=false;
+            for(int j=0;j<n;j++){
+                if(a[j]>a[i]) large=true;
+                if(a[j]<a[i]) small=true;
+            }
+            if(small && large) c++;
+        } */
+        
+        int mine=*min_element(a.begin(),a.end());
+        int maxe=*max_element(a.begin(),a.end());
+        
+        for(int i=0;i<n;i++){
+            if(a[i]>mine && a[i]<maxe) c++;
+        }
+        
+        return c; 
+    }
+};
